f99f26714b9114d87e8c02701a1a418e70ed365d
Gajendra Sahu <gajmain2020@gmail.com>
2025-06-10 12:36:25 +0530

    fix: logger for exception handling

--- app/utils/exception_handlers.py
+++ app/utils/exception_handlers.py
@@ -1,13 +1,32 @@
+import os
+import logging
 from fastapi import Request
-from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from app.utils.response import create_response
 
+os.makedirs("logs", exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("logs/logs.txt"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
 async def http_exception_handler(request: Request, exc: StarletteHTTPException):
-    return create_response(message=exc.detail, code=exc.status_code, error=True)
+    logger.error(f"HTTPException: {exc.detail} | Status Code: {exc.status_code} | Path: {request.url.path}")
+    return create_response(
+        message=exc.detail,
+        code=exc.status_code,
+        error=True
+    )
 
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    logger.error(f"ValidationError on {request.url.path}: {exc.errors()}")
     return create_response(
         message="Validation error",
         code=422,
